dea7b3b958c885bce1c7f2d180a1375138e2a38f
Finbarrs Oketunji <oketunjifinbarrs@gmail.com>
2018-06-28 20:15:44 -0400

    nfl fantasy stats

--- nfl_fantasy.py
+++ nfl_fantasy.py
@@ -13,7 +13,7 @@ page = urllib2.urlopen(url)
 soup = BeautifulSoup(page, 'html.parser')
 tbody = soup.find('tbody')
 fantasytable = soup.find(id="all_fantasy")
-fantasy_rankings = [h2.text for h2 in fantasytable.find_all('h2')]
+#fantasy_rankings = [h2.text for h2 in fantasytable.find_all('h2')]
 name = [td.text for td in tbody.find_all('td', attrs={"data-stat": "player"})]
 position_ranking = [td.text for td in tbody.find_all('td', attrs={"data-stat": "fantasy_rank_pos"})]
 overall_ranking = [td.text for td in tbody.find_all('td', attrs={"data-stat": "fantasy_rank_overall"})]
@@ -43,8 +43,7 @@ blank = " "
 # print date
 # print players fantasy stats
 
-row1 = zip(fantasy_rankings,blank,blank,blank,blank,blank,blank,blank,blank,blank,blank,blank,blank,blank,blank,blank,blank,blank,blank,blank,blank,blank,blank)
-row2 = zip(player,blank,blank,blank,blank,blank,blank,blank,blank,blank,blank,blank,blank,blank,blank,blank,blank,blank,blank,blank,blank,blank,blank)
+row1 = zip(player,blank,blank,blank,blank,blank,blank,blank,blank,blank,blank,blank,blank,blank,blank,blank,blank,blank,blank,blank,blank,blank,blank)
 rows = zip(name,position_ranking,overall_ranking,team,position,age,games_played,games_started,passes_completed,passes_attempted,passing_yards,passing_touchdowns,interceptions,rushing_attempts,rushing_yards,rushing_touchdowns,pass_targets,receptions,receiving_yards,receiving_touchdowns,fantasy_points,draftkings_points,fanduel_points)
 
 with open('nfl_fantasy.csv', 'wb') as f:
