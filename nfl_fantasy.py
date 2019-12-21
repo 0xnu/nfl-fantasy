914ac57c7c636742e7be675ba15f6726f0b11d21
Finbarrs Oketunji <oketunjifinbarrs@gmail.com>
2019-12-21 11:21:46 -0800

    2019 data
    Signed-off-by: Finbarrs Oketunji <oketunjifinbarrs@gmail.com>

--- nfl_fantasy.py
+++ nfl_fantasy.py
@@ -74,7 +74,6 @@ blank = " "
 
 # print title
 # print players fantasy stats
-
 row1 = zip(player,posrank,ovrank,tm,fantpos,ages,g,gs,comp,att,yds,td,intn,attn,ydsn,rushtd,tgt,rect,rectyds,rectdds,fantpt,dkpts,fdpts)
 rows = zip(name,position_ranking,overall_ranking,team,position,age,games_played,games_started,passes_completed,passes_attempted,passing_yards,passing_touchdowns,interceptions,rushing_attempts,rushing_yards,rushing_touchdowns,pass_targets,receptions,receiving_yards,receiving_touchdowns,fantasy_points,draftkings_points,fanduel_points)
 
